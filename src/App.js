03c326c56c63492dfcb36c4b23f1718cd2a48901
ankit-hindustani <namaste.ankitsingh@gmail.com>
2022-08-27 23:02:26 +0530

    fix: remove content

--- src/App.js
+++ src/App.js
@@ -3,23 +3,15 @@ import mobileDashboard from './dashboard.png';
 import './App.css';
 
 function App() {
-  var i = 0;
-  var txt = "Lorem ipsum dummy text blabla.";
-  var speed = 50;
-    setInterval(function(){
-      if (i < txt.length) {
-        document.getElementById("demo").innerHTML += txt.charAt(i);
-        i++;
-      }
-    }, speed);
+  
     
   return (
-    <div className="App">
+    <div style={{"textAlign":"center"}} className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <section className='main-section'>
-        <div className="">
+      {/* <section className='main-section'> */}
+        {/* <div className="">
           <h1 className="heading">
             Now get your perfect style at your favourite salon without any
             waiting!
@@ -27,15 +19,13 @@ function App() {
           <h1 className="heading-2">
             Coming soon...
           </h1>
-          {/* <p className="sub-heading">
-            Now get your perfect style at your favourite salon without any
-            waiting!
-          </p> */}
+          
         </div>
         <div className="">
           <img src={mobileDashboard} className="mobile-dashboard-img" alt="logo" />
-        </div>
-      </section>
+        </div> */}
+        <h1 className='heading' style={{"height":"100%","width":"100%","margin":"auto"}}>Stay tuned, we are coming soon...</h1>
+      {/* </section> */}
     </div>
   );
 }
